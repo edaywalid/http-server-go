894daf86273cd68dcaa760541415cf795c70c56c
edaywalid <becharwalid9@gmail.com>
2024-07-16 10:44:41 +0100

    set up a tcp server and accept incoming connections

--- server.go
+++ server.go
@@ -1,8 +1,30 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
 	port := flag.String("p", "8080", "Port to listen on")
 	flag.Parse()
+
+	address := fmt.Sprintf("0.0.0.0:%s", *port)
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Printf("Failed to bind to port %s\n", *port)
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+	}
 }
